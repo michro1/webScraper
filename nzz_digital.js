b1353703bbd9e70d67de7938e34148fab40fc329
Michelle Rosenberger Lomeli <michelle.rosenberger@hotmail.com>
2019-05-11 11:44:30 +0200

    Sort dates + install packages

--- nzz_digital.js
+++ nzz_digital.js
@@ -1,27 +1,21 @@
 [
   {
-    "title": "Wer hat Angst vor den Roboterjournalisten?",
-    "excerpt": "Eine KI-Software liefert Zusammenfassungen von wissenschaftlichen Aufsätzen. Dabei wird Fachchinesisch in Kauderwelsch übersetzt.",
-    "url": "https://www.nzz.ch/digital/wer-hat-angst-vor-den-roboterjournalisten-ld.1480687",
-    "date": "Thu, 09 May 2019 16:47:46 +0200"
-  },
-  {
     "title": "Bald werden Smartphones die Welt mit neuen Augen sehen",
     "excerpt": "Ein neuer Kamerasensor von Samsung bringt es auf eine Auflösung von gewaltigen 64 Megapixeln. Aufnahmen mit dem Smartphone werden damit noch detaillierter und gelingen auch bei wenig Licht.",
     "url": "https://www.nzz.ch/digital/bald-werden-smartphones-die-welt-mit-neuen-augen-sehen-ld.1480981",
     "date": "Fri, 10 May 2019 16:56:18 +0200"
   },
   {
+    "title": "Wer hat Angst vor den Roboterjournalisten?",
+    "excerpt": "Eine KI-Software liefert Zusammenfassungen von wissenschaftlichen Aufsätzen. Dabei wird Fachchinesisch in Kauderwelsch übersetzt.",
+    "url": "https://www.nzz.ch/digital/wer-hat-angst-vor-den-roboterjournalisten-ld.1480687",
+    "date": "Thu, 09 May 2019 16:47:46 +0200"
+  },
+  {
     "title": "Wie Streaming der Musikbranche Geld in die Kassen spült",
     "excerpt": "Youtube ist weltweit die populärste Plattform für Musikstreaming. Mit dem kostenpflichtigen Youtube Music will Google nun verstärkt zahlungswillige Abonnenten ansprechen.",
     "url": "https://www.nzz.ch/digital/wie-streaming-der-musikbranche-geld-in-die-kassen-spuelt-ld.1480670",
     "date": "Thu, 09 May 2019 16:31:13 +0200"
-  },
-  {
-    "title": "Auch bei Google wird die Privatsphäre neuerdings grossgeschrieben",
-    "excerpt": "An der diesjährigen Entwicklerkonferenz verkündet Google Verbesserungen beim Sprachassistenten und ein günstiges Hochleistungs-Smartphone. Vor allem aber gelobt die Firma besseren Datenschutz – der neue Trend im Silicon Valley.",
-    "url": "https://www.nzz.ch/wirtschaft/google-feiert-die-privatsphaere-ld.1480206",
-    "date": "Wed, 08 May 2019 06:01:14 +0200"
   },
   {
     "title": "«Google Nest Hub»: Eine Art «Bildschirm-Lautsprecher-Internet-Terminal-Smart-Home-Zentrale»",
@@ -35,16 +29,16 @@
     "date": "Wed, 08 May 2019 06:57:00 +0200"
   },
   {
+    "title": "Auch bei Google wird die Privatsphäre neuerdings grossgeschrieben",
+    "excerpt": "An der diesjährigen Entwicklerkonferenz verkündet Google Verbesserungen beim Sprachassistenten und ein günstiges Hochleistungs-Smartphone. Vor allem aber gelobt die Firma besseren Datenschutz – der neue Trend im Silicon Valley.",
+    "url": "https://www.nzz.ch/wirtschaft/google-feiert-die-privatsphaere-ld.1480206",
+    "date": "Wed, 08 May 2019 06:01:14 +0200"
+  },
+  {
     "title": "Kultige Klötzchen zwischen Sein und Schein",
     "excerpt": "Zum Jubiläum von Minecraft transportiert Microsoft das populäre Computerspiel in die wirkliche Welt. Auf dem Smartphone-Bildschirm lassen sich Computergrafik-Elemente mit realen Gegenständen kombinieren.",
     "url": "https://www.nzz.ch/digital/kultige-kloetzchen-zwischen-sein-und-schein-ld.1480049",
     "date": "Tue, 07 May 2019 15:59:09 +0200"
-  },
-  {
-    "title": "Facebook löscht die Profile von rechten Kommentatoren wie Alex Jones – Fanseiten sind weiterhin aufrufbar",
-    "excerpt": "Um stärker gegen Hass und Hetze auf der eigenen Plattform vorzugehen, hat Facebook die Konten des Verschwörungstheoretikers Alex Jones und anderer rechter Kommentatoren gesperrt. Das wirft die Frage auf, warum dies erst jetzt passiert.",
-    "url": "https://www.nzz.ch/feuilleton/medien/facebook-loescht-die-profile-von-rechten-kommentatoren-wie-alex-jones-fanseiten-sind-weiterhin-aufrufbar-ld.1479178",
-    "date": "Fri, 03 May 2019 11:50:08 +0200"
   },
   {
     "title": "Firefox-Bug blockiert Browser-Erweiterungen",
@@ -59,39 +53,10 @@
     "date": "Sun, 05 May 2019 13:01:18 +0200"
   },
   {
-    "title": "Wenn der Hacker Spitalpatienten mitbehandelt",
-    "excerpt": "Zunehmende Vernetzung, ungeschützte Medizinalgeräte und nicht mehr zeitgemässe IT machen Spitäler anfällig für Cyberattacken: Der Angriff mit «Wanna Cry» war dabei nur der letzte Schrei.",
-    "url": "https://www.nzz.ch/digital/computervirus-wanna-cry-wenn-der-hacker-mitbehandelt-ld.1294555",
-    "date": "Thu, 18 May 2017 14:28:24 +0200"
-  },
-  {
-    "title": "Der Code und das dilettantische Vorgehen erinnern Experten an Nordkorea",
-    "excerpt": "Wer steckt hinter dem weltweiten Cyber-Angriff? Experten suchen fieberhaft nach dem oder den Tätern. Nun kommt eine Nation ins Gespräch, die schon des Öfteren verdächtig war.",
-    "url": "https://www.nzz.ch/wirtschaft/cyberattacke-wanna-cry-experten-nordkorea-koennte-fuer-cyber-attacke-verantwortlich-sein-ld.1293620",
-    "date": "Tue, 16 May 2017 07:30:00 +0200"
-  },
-  {
-    "title": "Hacker wollen Swift-Bankdaten veröffentlichen",
-    "url": "https://www.nzz.ch/wirtschaft/schadsoftware-wanna-cry-hacker-drohen-mit-veroeffentlichung-von-bankdaten-ld.1293914",
-    "date": "Tue, 16 May 2017 19:18:25 +0200"
-  },
-  {
-    "title": "Sollte man Computer-Eigentümer bestrafen, die ihre Systeme nicht updaten?",
-    "excerpt": "Die Schadsoftware «Wanna Cry» nutzte eine Sicherheitslücke aus, die schon länger bekannt war. Nur Systeme, die das entsprechende Update nicht eingespielt hatten, waren gefährdet. Müsste man die Säumigen bestrafen?",
-    "url": "https://www.nzz.ch/leserdebatte/leserdebatte-sollte-man-computer-eigentuemer-bestrafen-die-ihre-systeme-nicht-updaten-ld.1293641",
-    "date": "Tue, 16 May 2017 10:17:05 +0200"
-  },
-  {
-    "title": "Von den Versäumnissen eingeholt",
-    "excerpt": "Im Gegensatz zum Ausland hat der Erpressertrojaner «WannaCry» in der Schweiz wenig Schaden angerichtet. Trotzdem kommen zum Teil haarsträubende Unterlassungen zum Vorschein.",
-    "url": "https://www.nzz.ch/wirtschaft/erstmals-ein-erpressungstrojaner-mit-wurm-von-den-versaeumnissen-eingeholt-ld.1293335",
-    "date": "Mon, 15 May 2017 20:17:36 +0200"
-  },
-  {
-    "title": "Das Ende der digitalen Sorglosigkeit",
-    "excerpt": "Angriffe auf Computer im Parlament oder Router der Telekom: Deutschland rüstet im Kampf gegen Cyberattacken auf. Auch die Wirtschaft macht mit: DAX- und andere Firmen sind Teil einer neuen Initiative.",
-    "url": "https://www.nzz.ch/wirtschaft/deutschland-reagiert-auf-angriffe-das-ende-der-digitalen-sorglosigkeit-ld.1293571",
-    "date": "Mon, 15 May 2017 20:25:43 +0200"
+    "title": "Facebook löscht die Profile von rechten Kommentatoren wie Alex Jones – Fanseiten sind weiterhin aufrufbar",
+    "excerpt": "Um stärker gegen Hass und Hetze auf der eigenen Plattform vorzugehen, hat Facebook die Konten des Verschwörungstheoretikers Alex Jones und anderer rechter Kommentatoren gesperrt. Das wirft die Frage auf, warum dies erst jetzt passiert.",
+    "url": "https://www.nzz.ch/feuilleton/medien/facebook-loescht-die-profile-von-rechten-kommentatoren-wie-alex-jones-fanseiten-sind-weiterhin-aufrufbar-ld.1479178",
+    "date": "Fri, 03 May 2019 11:50:08 +0200"
   },
   {
     "title": "Deepfakes: Kann ich überhaupt noch glauben, was ich sehe?",
@@ -110,5 +75,40 @@
     "excerpt": "Januar 1979: Nach monatelangen Protesten flieht Mohammad Reza Pahlevi – der Schah von Persien – aus Iran. Im Laufe des Jahres übernehmen die Mullahs unter der Führung Ajatollah Khomeinys die Macht im Land.",
     "url": "https://www.nzz.ch/international/die-islamische-revolution-1979-irans-schicksalsjahr-ld.1451574",
     "date": "Wed, 16 Jan 2019 05:00:00 +0100"
+  },
+  {
+    "title": "Wenn der Hacker Spitalpatienten mitbehandelt",
+    "excerpt": "Zunehmende Vernetzung, ungeschützte Medizinalgeräte und nicht mehr zeitgemässe IT machen Spitäler anfällig für Cyberattacken: Der Angriff mit «Wanna Cry» war dabei nur der letzte Schrei.",
+    "url": "https://www.nzz.ch/digital/computervirus-wanna-cry-wenn-der-hacker-mitbehandelt-ld.1294555",
+    "date": "Thu, 18 May 2017 14:28:24 +0200"
+  },
+  {
+    "title": "Hacker wollen Swift-Bankdaten veröffentlichen",
+    "url": "https://www.nzz.ch/wirtschaft/schadsoftware-wanna-cry-hacker-drohen-mit-veroeffentlichung-von-bankdaten-ld.1293914",
+    "date": "Tue, 16 May 2017 19:18:25 +0200"
+  },
+  {
+    "title": "Sollte man Computer-Eigentümer bestrafen, die ihre Systeme nicht updaten?",
+    "excerpt": "Die Schadsoftware «Wanna Cry» nutzte eine Sicherheitslücke aus, die schon länger bekannt war. Nur Systeme, die das entsprechende Update nicht eingespielt hatten, waren gefährdet. Müsste man die Säumigen bestrafen?",
+    "url": "https://www.nzz.ch/leserdebatte/leserdebatte-sollte-man-computer-eigentuemer-bestrafen-die-ihre-systeme-nicht-updaten-ld.1293641",
+    "date": "Tue, 16 May 2017 10:17:05 +0200"
+  },
+  {
+    "title": "Der Code und das dilettantische Vorgehen erinnern Experten an Nordkorea",
+    "excerpt": "Wer steckt hinter dem weltweiten Cyber-Angriff? Experten suchen fieberhaft nach dem oder den Tätern. Nun kommt eine Nation ins Gespräch, die schon des Öfteren verdächtig war.",
+    "url": "https://www.nzz.ch/wirtschaft/cyberattacke-wanna-cry-experten-nordkorea-koennte-fuer-cyber-attacke-verantwortlich-sein-ld.1293620",
+    "date": "Tue, 16 May 2017 07:30:00 +0200"
+  },
+  {
+    "title": "Das Ende der digitalen Sorglosigkeit",
+    "excerpt": "Angriffe auf Computer im Parlament oder Router der Telekom: Deutschland rüstet im Kampf gegen Cyberattacken auf. Auch die Wirtschaft macht mit: DAX- und andere Firmen sind Teil einer neuen Initiative.",
+    "url": "https://www.nzz.ch/wirtschaft/deutschland-reagiert-auf-angriffe-das-ende-der-digitalen-sorglosigkeit-ld.1293571",
+    "date": "Mon, 15 May 2017 20:25:43 +0200"
+  },
+  {
+    "title": "Von den Versäumnissen eingeholt",
+    "excerpt": "Im Gegensatz zum Ausland hat der Erpressertrojaner «WannaCry» in der Schweiz wenig Schaden angerichtet. Trotzdem kommen zum Teil haarsträubende Unterlassungen zum Vorschein.",
+    "url": "https://www.nzz.ch/wirtschaft/erstmals-ein-erpressungstrojaner-mit-wurm-von-den-versaeumnissen-eingeholt-ld.1293335",
+    "date": "Mon, 15 May 2017 20:17:36 +0200"
   }
 ]
--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 // Authors: Valery Bamberger, Claudia List, Michelle Rosenberger
 // Date:    May 10, 2019
 
-let Parser = require('rss-parser');
-let parser = new Parser();
+let fs      = require("fs");
+let Parser  = require('rss-parser');
+let parser  = new Parser();
 
-let fs = require("fs");
- 
 (async () => {
 
-  // Access url and extract information
+  // Define .rss feed
   let feed = await parser.parseURL('https://www.nzz.ch/digital.rss');
 
   // Create empty array
@@ -18,17 +17,27 @@ let fs = require("fs");
   // Loop for information
   feed.items.forEach(item => {
 
-    // define objects & save information in variables
-    let article_object = {title:    item.title,
-                          excerpt:  item.content,
-                          url:      item.link,
-                          date:     item.pubDate};
+    // Define objects and save information
+    let article_object = {
+      title:    item.title,
+      excerpt:  item.content,
+      url:      item.link,
+      date:     item.pubDate
+    };
 
     // Populate array
     article_array.push(article_object);
-
-    
+  
   });
+
+  // Sort dates
+  article_array.sort((a, b) => {
+    if (Date.parse(a.date) > Date.parse(b.date)) {
+      return -1;
+    } else {
+      return 1;
+    }
+                })
 
   // Write output to nzz_digital.js
   fs.writeFile("nzz_digital.js", JSON.stringify(article_array, null, 2), function(err) {
@@ -36,6 +45,7 @@ let fs = require("fs");
         return console.log(err);
     }
     console.log("Nzz_digitial file saved successfully!");
+    
   });
  
 })();
